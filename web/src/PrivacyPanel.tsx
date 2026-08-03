dd41c2a870cc0c75901a38f971239e5449150bd7
robihamanto <rrobie44@gmail.com>
2026-08-03 20:43:59 +0700

    Web: Use encryption custom domain

--- web/src/PrivacyPanel.tsx
+++ web/src/PrivacyPanel.tsx
@@ -39,7 +39,7 @@ export function PrivacyPanel({
       </div>
       <a
         className="privacy-details-link"
-        href="https://heritg-encryption.vercel.app"
+        href="https://heritg-encryption.hamanto.com"
         rel="noopener noreferrer"
         target="_blank"
       >
